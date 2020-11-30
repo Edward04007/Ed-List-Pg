dffcce99e39e10f7969e50a1b43999ae4ba4f9d2
Éverton <foreverplayer95@gmail.com>
2020-11-30 18:11:23 -0300

    teste conexão

--- database/data/user.php
+++ database/data/user.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 require_once(__DIR__."/../env/connect.php");
-$user =filter_input(INPUT_POST,'user', FILTER_SANITIZE_STRING);
+ echo $connect;
+?>
+<!-- $user =filter_input(INPUT_POST,'user', FILTER_SANITIZE_STRING);
 $pass = filter_input(INPUT_POST,'pass', FILTER_SANITIZE_STRING);
 $senha_sql = sha1($pass);
 
@@ -18,5 +20,4 @@ if(pg_num_rows($consulta) > 0){
         }
     }else{
         header('Location:../../index.php');
-    }
-?>
+    } -->
