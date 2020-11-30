c53a69d4fdb4ad47f5f7cf9f602b5d597b311638
Éverton <foreverplayer95@gmail.com>
2020-11-30 17:29:27 -0300

    teste do banco Postgres

--- database/env/connect.php
+++ database/env/connect.php
@@ -3,5 +3,10 @@ $host = getenv('DATABASE_URL');
 $user = getenv('USER');
 $password = getenv('PASS');
 $database = getenv('DBNAME');
-$connect = pg_connect($host, $user, $password, $database)
+$connect = pg_connect($host, $user, $password, $database);
+if($connect){
+    echo"conexão 100%";
+}else{
+    echo"erro na conexão";
+}
 ?>
