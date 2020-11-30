3f67742d17fe0c5f011abc1c52391581ceb95165
Éverton <foreverplayer95@gmail.com>
2020-11-30 18:16:59 -0300

    env

--- database/env/connect.php
+++ database/env/connect.php
@@ -1,9 +1,10 @@
 <?php
-$host = getenv('DATABASE_URL');
-$user = getenv('USER');
-$password = getenv('PASS');
-$database = getenv('DBNAME');
+$host = $_ENV['DATABASE_URL'];
+$user = $_ENV['USER'];
+$password = $_ENV['PASS'];
+$database = $_ENV['DBNAME'];
 $connect = pg_connect($host, $user, $password, $database);
+
 if($connect){
     echo"conexão 100%";
 }else{
