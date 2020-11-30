4c462cb9f9d85d9ec3d617bcb4f47b1eebe9433d
Éverton <foreverplayer95@gmail.com>
2020-11-30 18:26:59 -0300

    conexão01

--- database/env/connect.php
+++ database/env/connect.php
@@ -5,9 +5,4 @@ $password = $_ENV['PASS'];
 $database = $_ENV['DBNAME'];
 $connect = pg_connect($host, $user, $password, $database);
 
-if($connect){
-    echo"conexão 100%";
-}else{
-    echo"erro na conexão";
-}
 ?>
