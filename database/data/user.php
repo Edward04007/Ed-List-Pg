39e04844b8d0fc41449401bc4ac1f90aae85c77d
Éverton <foreverplayer95@gmail.com>
2020-11-30 22:36:43 -0300

    conexão18

--- database/data/user.php
+++ database/data/user.php
@@ -8,7 +8,7 @@ $senha_sql = sha1($pass);
 $sql = "SELECT senha, nome FROM tb_aluno WHERE nome = '$user' AND senha = '$senha_sql'";
 $consulta = pg_query($connect, $sql);
 if(pg_num_rows($consulta) > 0){
-    $sql = "SELECT senha, id, nome, foto FROM tb_aluno WHERE nome = '$user' AND senha = '$senha_sql'";
+    $sql = "SELECT senha, pk_aluno, nome, foto FROM tb_aluno WHERE nome = '$user' AND senha = '$senha_sql'";
     $consulta = pg_query($connect, $sql);
         if($row = pg_fetch_array($consulta)){
             $_SESSION['id-aluno'] = $row['id'];
