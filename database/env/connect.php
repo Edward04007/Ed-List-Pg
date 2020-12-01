fac3e2ec2e147b1b1b1679b1af97c02830609b9a
Éverton <foreverplayer95@gmail.com>
2020-11-30 21:40:49 -0300

    conexão13

--- database/env/connect.php
+++ database/env/connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = getenv('HOST');
 $database = getenv('DBNAME');
-$port = gent('PORT');
+$port = getenv('PORT');
 $usuario = getenv('db_user');
 $password = getenv('PASS');
 $connect = pg_connect($host, $database, $usuario, $port, $password);
