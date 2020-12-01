92f6f0a3c0710ba2756ecd20e31b6b7233c55233
Éverton <foreverplayer95@gmail.com>
2020-11-30 22:11:50 -0300

    conexão16

--- database/env/connect.php
+++ database/env/connect.php
@@ -4,7 +4,7 @@ $usuario = getenv('db_user');
 $password = getenv('PASS');
 $database = getenv('DBNAME');
 $port = getenv('PORT');
-$connect = pg_connect($host, $usuario,$password, $database,);
+$connect = pg_connect(getenv("DATABASE_URL"));
 if($connect){
     var_dump($connect);
 }else{
