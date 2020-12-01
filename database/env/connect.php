c9dee5c046ae36699517ca0dafcac912df5a2aac
Éverton <foreverplayer95@gmail.com>
2020-11-30 21:48:08 -0300

    conexão14

--- database/env/connect.php
+++ database/env/connect.php
@@ -1,10 +1,9 @@
 <?php
-$host = getenv('HOST');
-$database = getenv('DBNAME');
-$port = getenv('PORT');
-$usuario = getenv('db_user');
-$password = getenv('PASS');
-$connect = pg_connect($host, $database, $usuario, $port, $password);
+$host = 'ec2-54-159-107-189.compute-1.amazonaws.com';
+$database = 'd7hgi3rq02v1ij';
+$usuario = 'xygnpfhyqyhnnz';
+$password = 'a9b1bedbc2c69bb3df3f1dff8e4b00e02df50a934e1634ba3575fb9daca30a5c';
+$connect = pg_connect($host, $database, $usuario, $password);
 if($connect){
     var_dump($connect);
 }else{
