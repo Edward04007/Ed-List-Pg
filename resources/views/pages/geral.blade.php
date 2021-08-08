b1599c8acaed132e5f61499d50ca37d2440dcd62
Éverton <foreverplayer95@gmail.com>
2021-08-08 08:25:13 -0300

    Atualizando horario e deps

--- resources/views/pages/geral.blade.php
+++ resources/views/pages/geral.blade.php
@@ -9,50 +9,46 @@
             <div class="div-form">
             <span class="font">Segunda-feira</span>
                 <ul>
-                    <li class="font">Química - 08:00 ás 09:30</li>
-                    <li class="font">História - 10:00 ás 11:30</li>
-                    <li class="font">Matemática - 13:30 ás 15:00</li>
-                    <li class="font">Lab.Software - 15:30 ás 17:00</li>
+                    <li class="font">Estágio - 08:00 ás 11:30</li>
+                    <li class="font">História - 13:30 ás 15:00</li>
+                    <li class="font">Matemática - 15:30 ás 17:00</li>
                 </ul>
             </div>
             <div class="div-form">
             <span class="font">Terça-feira</span>
                 <ul>
-                    <li class="font">Geografia - 08:00 ás 09:30</li>
-                    <li class="font">Lab.Web - 10:00 ás 11:30</li>
-                    <li class="font">Lab.Hardware - 13:30 ás 15:00</li>
-                    <li class="font">Lab.Software - 15:30 ás 17:00</li>
+                    <li class="font">Estágio - 08:00 ás 11:30</li>
+                    <li class="font">Geografia - 13:30 ás 15:00</li>
+                    <li class="font">Química - 15:30 ás 17:00</li>
                 </ul>
             </div>
             <div class="div-form">
             <span class="font">Quarta-feira</span>
                 <ul>
-                    <li class="font">Ed.Física - 08:00 ás 08:45</li>
-                    <li class="font">Sociologia - 08:45 ás 09:30</li>
-                    <li class="font">Redação - 10:00 ás 11:30</li>
-                    <li class="font">Espanhol - 13:30 ás 14:15</li>
+                    <li class="font">Estágio - 08:00 ás 11:30</li>
+                    <li class="font">Sociologia - 13:30 ás 14:15</li>
                     <li class="font">Inglês - 14:15 ás 15:00</li>
-                    <li class="font">Lab.Hardware - 15:30 ás 17:00</li>
+                    <li class="font">Ed.Física -  15:30 ás 16:15</li>
+                    <li class="font">Espanhol - 16:15 ás 17:00</li>
                 </ul>
             </div>
             <div class="div-form">
             <span class="font">Quinta-feira</span>
                 <ul>
-                    <li class="font">Física - 08:00 ás 09:30</li>
-                    <li class="font">P.Formação - 10:00 ás 11:30</li>
-                    <li class="font">Lab.Web - 13:30 ás 15:00</li>
-                    <li class="font">Português - 15:30 ás 16:15</li>
-                    <li class="font">Projeto de vida - 16:15 ás 17:00</li>
+                    <li class="font">Estágio - 08:00 ás 11:30</li>
+                    <li class="font">Física - 13:30 ás 15:00</li>
+                    <li class="font">Redação - 15:30 ás 17:00</li>
                 </ul>
             </div>
             <div class="div-form">
             <span class="font">Sexta-feira</span>
                 <ul>
-                    <li class="font">Biologia - 08:00 ás 09:30</li>
-                    <li class="font">Português - 10:00 ás 11:30</li>
-                    <li class="font">Matemática - 13:30 ás 15:00</li>
-                    <li class="font">Filosofia - 15:30 ás 16:15</li>
-                    <li class="font">F.Cidadania - 16:15 ás 17:00</li>
+                    <li class="font">Rev. Química - 08:00 ás 08:45</li>
+                    <li class="font">F.P.Cidadania - 08:45 ás 09:30</li>
+                    <li class="font">Biologia - 10:00 ás 11:30</li>
+                    <li class="font">Matemática - 13:30 ás 14:15</li>
+                    <li class="font">Filosia - 14:15 ás 15:00</li>
+                    <li class="font">Português - 15:30 ás 17:00</li>
                 </ul>
             </div>
         </div>
